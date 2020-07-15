07f9586292e880668dc84d24734ce56e09eabe34
Saood-Ahmed <59747007+Saood-Ahmed@users.noreply.github.com>
2020-07-15 12:46:05 +0500

    Updated Project Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import alice from './img/alice.gif';
+import useWebAnimations from '@wellyshen/use-web-animations';
 
+//importing other gifs
+import Clouds from './Clouds';
+//importing Bottles
+import b_1 from './img/bottles/1.png';
+import b_2 from './img/bottles/2.png';
+import b_3 from './img/bottles/3.png';
+import b_4 from './img/bottles/4.png';
+  
 function App() {
+
+  var bottle = b_1;
+  var count = 0;
+
+
+  const{ ref, getAnimation } = useWebAnimations({
+    keyframes:[
+    { transform: 'translateX(0px)' },
+    { transform: 'translateX(1200px)' }
+  ], 
+  timing: {
+    duration:5000,
+    iterations: Infinity
+  }
+})
+
+  const speedUp = () => {
+    const animation = getAnimation();
+    animation.updatePlaybackRate(animation.playbackRate < 2 ? animation.playbackRate * 1.25 : animation.playbackRate);
+    
+  }
+
+  const slowDown = (e) => {
+    const animation = getAnimation();
+    if(count === 0) {
+      e.target.src = b_2;
+      count ++;  
+      animation.updatePlaybackRate(animation.playbackRate * 0.5);
+      document.getElementById('alice').style.width = '20%';
+      document.getElementById('alice').style.marginTop = '22%';
+    }
+    else if(count === 1) {
+      e.target.src = b_3;
+      count++;  
+      animation.updatePlaybackRate(animation.playbackRate * 0.35);
+      document.getElementById('alice').style.width = '15%';
+      document.getElementById('alice').style.marginTop = '25%';
+    }
+    else if(count === 2) {
+      e.target.src = b_4;
+      count++;
+      animation.updatePlaybackRate(animation.playbackRate * 0.25);
+      document.getElementById('alice').style.width = '12.5%';
+      document.getElementById('alice').style.marginTop = '28%';
+    }
+    else{
+      e.target.src = bottle;
+      count = 0;
+      animation.updatePlaybackRate(animation.playbackRate += 1);
+      document.getElementById('alice').style.width = '25%';
+      document.getElementById('alice').style.marginTop = '17%';
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clouds />
+      <div  className='alice'>
+        <img ref={ ref } src={alice} alt="alice" onClick= { speedUp } id="alice"/>
+        
+        <img src={bottle} alt='bottle 1' onClick={ slowDown }/>
+       
+      </div>
     </div>
   );
 }
---
+++ src/Clouds.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import cloud from './img/cloud.png';
+import useWebAnimations from '@wellyshen/use-web-animations';
+
+function Clouds() {
+
+  const{ ref, getAnimation } = useWebAnimations({
+    keyframes:[
+    { transform: 'translateX(-400px)' },
+    { transform: 'translateX(1200px)' }
+  ], 
+  timing: {
+    duration:8000,
+    iterations: Infinity
+  }
+})
+
+const speedUp = () => {
+  const animation = getAnimation();
+  animation.updatePlaybackRate(animation.playbackRate < 3 ? animation.playbackRate * 1.25 : animation.playbackRate)
+}
+
+  return (
+    <div>
+      <div ref={ ref }  className='clouds' onClick={ speedUp }>
+        <img  src={cloud} alt="cloud"/>
+        <img  src={cloud} alt="cloud"/>
+      </div>
+    </div>
+  );
+}
+
+export default Clouds;
